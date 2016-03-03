ccad6eb4f244281cf079fb98621d04739852169f
joblolin95 <blaineo_95@yahoo.com>
2016-03-03 13:11:51 -0500

    Created a model and used it to display name and fake phone and email information via the home controller and the index and contact views

--- JosephOlin.Web/Controllers/HomeController.cs
+++ JosephOlin.Web/Controllers/HomeController.cs
@@ -3,21 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JosephOlin.Web.Models;
 
 namespace JosephOlin.Web.Controllers
 {
     public class HomeController : Controller
     {
+        Person p = new Person
+        {
+            Name = "Joseph Olin",
+            Email = "jbo@user.com",
+            Phone = "123-456-7890"
+        };
+        
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(p);
 
         }
 
         public ActionResult Contact()
         {
-            return View();
+            return View(p);
         }
     }
 }
