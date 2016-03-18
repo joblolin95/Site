7c37cd8112f7b61fe144ccbd2131c982a48b0928
joblolin95 <blaineo_95@yahoo.com>
2016-03-18 10:44:48 -0400

    Passed a Person object to the NotFound view, so that it can use the primary shared layout. However, this Person object is just another instance with the same data as the Person object in HomeController

--- JosephOlin.Web/Controllers/ErrorController.cs
+++ JosephOlin.Web/Controllers/ErrorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JosephOlin.Web.Models;
 
 namespace JosephOlin.Web.Controllers
 {
@@ -12,7 +13,19 @@ namespace JosephOlin.Web.Controllers
         public ActionResult NotFound()
         {
             Response.StatusCode = 404;
-            return View();
+            
+            //Creating another Person object is temporary
+            // A different implementation will eventually be followed.
+            return View(
+                new Person {
+                    Name = "Joseph Olin",
+                    Email = "jbo@user.com",
+                    Phone = "123-456-7890",
+                    Employer = "ZirMed, Inc",
+                    Position = "IT Intern",
+                    University = "Indiana University Southeast",
+                    Major = "Computer Science"
+                });
         }
     }
 }
